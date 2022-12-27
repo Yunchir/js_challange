a7bc52126cd58219f186936590f903c3ac52b931
Дамбийням У <dambii5655@gmail.com>
2022-12-27 09:37:51 +0800

    a

--- work_file-fetch/js/app.js
+++ work_file-fetch/js/app.js
@@ -1,22 +1,37 @@
-const select = document.querySelector('#breeds');
-const card = document.querySelector('.card');
+const select = document.querySelector("#breeds");
+const card = document.querySelector(".card");
 let current = "";
 
-fetch("https://dog.ceo/api/breeds/list") 
-.then((res) => res.json())
-.then((data) => {
-    generateBreeds(data.message[0])
-    fetchImage(data.message)
-});
+fetch("https://dog.ceo/api/breeds/list")
+  .then((res) => res.json())
+  .then((data) => {
+    generateBreeds(data.message);
+    // console.log(data);
+    fetchImage(data.message[0]);
+    current = data.message[0];
+  });
 
 function generateBreeds(data) {
-    data.map((dogBreed) => {
+  data.map((dogBreed) => {
     const html = `<option value="${dogBreed}">${dogBreed}</option>`;
     select.innerHTML += html;
-    });
+  });
 }
 
 function generateImage(url) {
-    const image = `<img src="${url}">`
-    card.innerHTML = image;
+  const image = `<img src="${url}">`;
+  card.innerHTML = image;
 }
+
+function fetchImage(breedType) {
+  fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+    .then((res) => res.json())
+    .then((data) => generateImage(data.message));
+}
+
+select.addEventListener("change", () => {
+  fetchImage(select.value);
+  current = select.value;
+});
+
+card.addEventListener("click", () => fetchImage(current));
